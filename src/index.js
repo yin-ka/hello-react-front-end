327ac8307a3730e570ca6af064d7cf4760cec9b6
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-11-04 18:40:45 +0100

    Refactor greetings fetch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './components/store';
+import store from './redux/store';
 
 function Index() {
   return (
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   );
 }
